ecf37836ffa65187c33d44dbcb1e263e817f650b
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2018-02-11 11:05:46 +0800

    .NET OWIN Identity Debut

---
+++ Monster/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace Monster
+{
+    public class Startup
+    {
+        private readonly string _applicationName;
+
+        public Startup()
+        {
+            _applicationName = ConfigurationManager.AppSettings["ApplicationName"] ?? "Monster";
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = $"{_applicationName}Cookie",
+                LoginPath = new PathString("/Account/Login"),
+                CookieName = $".{_applicationName}.Cookie"
+            });
+        }
+    }
+}
