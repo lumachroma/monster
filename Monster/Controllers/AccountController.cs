5ddb686ecbbe787381ed751db155714f465973ad
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2018-02-12 14:49:35 +0800

    User login using Firebase User Entity

--- Monster/Controllers/AccountController.cs
+++ Monster/Controllers/AccountController.cs
@@ -1,28 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using FirebaseRest;
+using FirebaseRest.Models;
+using Monster.Firebase;
+using Monster.Models;
 
 namespace Monster.Controllers
 {
     public class AccountController : Controller
     {
         private readonly string _applicationName;
-        private readonly UserModel _defaultUserAdmin;
+        private readonly FirebaseDataContext<User> _userModelContext;
 
         public AccountController()
         {
             _applicationName = ConfigurationManager.AppSettings["ApplicationName"] ?? "Monster";
-            _defaultUserAdmin = new UserModel
-            {
-                Username = "lumachroma",
-                Password = "123456qwerty!",
-                Email = "lumachroma@outlook.com",
-                State = "Kuala Lumpur",
-                Country = "Malaysia",
-                Roles = new List<string> { "Admin", "Developer", "Geek" }
-            }; //TODO: refactor
+
+            var firebaseUrl = ConfigurationManager.AppSettings["FirebaseUrl"] ?? "https://swag-monster.firebaseio.com";
+            var firebaseAuth = ConfigurationManager.AppSettings["FirebaseAuth"] ??
+                               "MfX7DaAWOUjr0zJ2invYbaX6UceHZ3vrif0VGeL4";
+            var firebaseQuery = new FirebaseQuery(new FirebaseClient(firebaseUrl, firebaseAuth));
+            _userModelContext = new FirebaseDataContext<User>("Users", firebaseQuery);
         }
 
         [HttpGet]
@@ -44,7 +47,7 @@ namespace Monster.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(LoginModel model, string returnUrl = "/")
+        public async Task<ActionResult> Login(LoginModel model, string returnUrl = "/")
         {
             if (string.IsNullOrEmpty(model.Username))
                 return RedirectToAction("Login", "Account",
@@ -53,29 +56,33 @@ namespace Monster.Controllers
                 return RedirectToAction("Login", "Account",
                     new { success = false, status = "Password cannot be set to null or empty." });
 
-            if (model.Username.Equals(_defaultUserAdmin.Username) &&
-                model.Password.Equals(_defaultUserAdmin.Password)) //TODO: refactor
+            var users = await _userModelContext.SearchByKeyValueAsync("\"Username\"", $"\"{model.Username}\"");
+            if (users.Any())
             {
-                var identity = new ClaimsIdentity($"{_applicationName}Cookie");
-
-                identity.AddClaim(new Claim(ClaimTypes.Name, _defaultUserAdmin.Username));
-                identity.AddClaim(new Claim(ClaimTypes.Email, _defaultUserAdmin.Email));
-                identity.AddClaim(new Claim(ClaimTypes.StateOrProvince, _defaultUserAdmin.State));
-                identity.AddClaim(new Claim(ClaimTypes.Country, _defaultUserAdmin.Country));
-
-                var roles = new List<Claim>();
-                foreach (var role in _defaultUserAdmin.Roles)
+                var user = users.First().Object;
+                if (model.Password == user.Password) //TODO: refactor
                 {
-                    var claim = new Claim(ClaimTypes.Role, role);
-                    roles.Add(claim);
+                    var identity = new ClaimsIdentity($"{_applicationName}Cookie");
+
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.Username));
+                    identity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+                    identity.AddClaim(new Claim(ClaimTypes.StateOrProvince, user.State));
+                    identity.AddClaim(new Claim(ClaimTypes.Country, user.Country));
+
+                    var roles = new List<Claim>();
+                    foreach (var role in user.Roles)
+                    {
+                        var claim = new Claim(ClaimTypes.Role, role);
+                        roles.Add(claim);
+                    }
+
+                    identity.AddClaims(roles);
+
+                    HttpContext.GetOwinContext().Authentication.SignIn(identity);
+
+                    if (returnUrl == "/") return RedirectToAction("Dashboard", "Home");
+                    return Redirect(returnUrl);
                 }
-
-                identity.AddClaims(roles);
-
-                HttpContext.GetOwinContext().Authentication.SignIn(identity);
-
-                if (returnUrl == "/") return RedirectToAction("Dashboard", "Home");
-                return Redirect(returnUrl);
             }
 
             return RedirectToAction("Login", "Account", new { success = false, status = "Wrong username or password." });
@@ -86,15 +93,5 @@ namespace Monster.Controllers
     {
         public string Username { get; set; }
         public string Password { get; set; }
-    }
-
-    public class UserModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public string State { get; set; }
-        public string Country { get; set; }
-        public List<string> Roles { get; set; } = new List<string>();
     }
 }
---
+++ Monster/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using FirebaseRepository;
+
+namespace Monster.Models
+{
+    public class User : IFirebaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+
+        public string ChangedBy { get; set; }
+        public DateTime ChangedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Id { get; set; }
+        public string WebId { get; set; }
+    }
+}
