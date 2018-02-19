b73ceb020ad6fbe84cf17a395ef15feec1d09c00
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2018-02-19 22:55:59 +0800

    Configure bundles, bundles optimization and compilation debug mode

--- Monster/App_Start/BundleConfig.cs
+++ Monster/App_Start/BundleConfig.cs
@@ -20,12 +20,20 @@ namespace Monster
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                        "~/Scripts/knockout-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                        "~/Scripts/moment*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/site.css"));
+
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
