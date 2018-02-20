59d78a5578defda25de853a857ce8bac760f4964
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2018-02-20 21:34:26 +0800

    Refactor KOPassword accepting dynamic endpoints and redirects

--- Monster/Scripts/KOScripts/KOPassword.js
+++ Monster/Scripts/KOScripts/KOPassword.js
@@ -2,17 +2,26 @@
     model: undefined,
     action: "Action",
     id: undefined,
+    endpoint: undefined,
+    redirect: undefined,
     isBusy: ko.observable(false),
     entity: ko.observable(),
-    viewDetails: function (model, action, id = null) {
+    viewDetails: function (model, action,
+        id = undefined,
+        endpoint = undefined,
+        redirect = undefined) {
         var thisObj = this;
         thisObj.model = model;
         thisObj.action = action.toLowerCase();
         thisObj.id = id;
+        thisObj.endpoint = endpoint;
+        thisObj.redirect = redirect;
         var koModel = `Ko${model}`;
+        var endpointUrl = thisObj.buildEndpointUrl(`/${thisObj.model}/Get/${thisObj.id}`);
+        var redirectUrl = thisObj.buildRedirectUrl(`/${thisObj.model}`);
         if (null != id) {
             thisObj.isBusy(true);
-            get(`/${thisObj.model}/Get/${thisObj.id}`, true, {})
+            get(endpointUrl, true, {})
                 .done(function (result) {
                     console.log(result);
                     thisObj.entity(new window[koModel](result));
@@ -20,8 +29,9 @@
                 }).fail(function (e) {
                     console.log(e.status);
                     console.log(e.statusText);
+                    console.log(e.responseText);
                     thisObj.isBusy(false);
-                    window.location.href = `/${thisObj.model}`;
+                    window.location.href = redirectUrl;
                 });
         } else {
             thisObj.entity(new window[koModel](guid()));
@@ -29,11 +39,18 @@
     },
     setEntityPasssword: function (thisObj) {
         var data = ko.toJSON(thisObj.entity);
-        var endpoint = `/${thisObj.model}/SetPassword/${thisObj.id}`;
-        var redirect = `/${thisObj.model}`;
+        var endpoint = thisObj.buildEndpointUrl(`/${thisObj.model}/SetPassword/${thisObj.id}`);
+        var redirect = thisObj.buildRedirectUrl(`/${thisObj.model}`);
         var msg = "Successfully set password.";
-        //console.log(data);
         thisObj.defultOperationEndpoint(data, endpoint, "PATCH", redirect, msg);
+    },
+    buildEndpointUrl: function (defaultUrl) {
+        var thisObj = this;
+        return (undefined != thisObj.endpoint) ? thisObj.endpoint : defaultUrl;
+    },
+    buildRedirectUrl: function (defaultUrl) {
+        var thisObj = this;
+        return (undefined != thisObj.redirect) ? thisObj.redirect : defaultUrl;
     },
     defultOperationEndpoint: function (json, endpoint, verb, redirect, successMessage) {
         var thisObj = this;
@@ -42,23 +59,13 @@
             .done(function (result) {
                 console.log(result);
                 thisObj.isBusy(false);
-                //app.showMessage(successMessage, config.application_name, ["OK"])
-                //    .done(function (result) {
-                //        if (result == "OK") {
                 window.location.href = redirect;
-                //        }
-                //    });
             }).fail(function (e) {
                 console.log(e.status);
                 console.log(e.statusText);
                 console.log(e.responseText);
                 thisObj.isBusy(false);
-                //app.showMessage(failMessage, config.application_name, ["OK"])
-                //    .done(function (result) {
-                //        if (result == "OK") {
                 window.location.href = redirect;
-                //        }
-                //    });
             });
     }
 };
