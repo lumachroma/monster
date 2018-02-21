ce79f82777c608d8def0073082f680845bbec354
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2018-02-21 09:57:58 +0800

    Refactor KOAll accepting dynamic endpoints

--- Monster/Scripts/KOScripts/KOAll.js
+++ Monster/Scripts/KOScripts/KOAll.js
@@ -1,12 +1,15 @@
 ﻿var modelView = {
     model: undefined,
+    endpoint: undefined,
     isBusy: ko.observable(false),
     list: ko.observableArray([]),
-    viewAll: function (model) {
+    viewAll: function (model, endpoint = undefined) {
         var thisObj = this;
         thisObj.model = model;
+        thisObj.endpoint = endpoint;
         thisObj.isBusy(true);
-        get(`/${thisObj.model}/All`, true, {})
+        var endpointUrl = thisObj.buildEndpointUrl(`/${thisObj.model}/All`);
+        get(endpointUrl, true, {})
             .done(function (result) {
                 console.log(result);
                 thisObj.list(result);
@@ -16,5 +19,12 @@
                 console.log(e.statusText);
                 thisObj.isBusy(false);
             });
+    },
+    buildEndpointUrl: function (defaultUrl) {
+        var thisObj = this;
+        return (undefined != thisObj.endpoint) ? thisObj.endpoint : defaultUrl;
+    },
+    refresh: function (thisObj) {
+        thisObj.viewAll(thisObj.model, thisObj.endpoint);
     }
 };
